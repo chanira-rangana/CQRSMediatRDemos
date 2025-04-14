6c311b9713f98aa3635b0c1493315f7a5e591e92
chanira-rangana <chanirapannala@gmail.com>
2025-04-14 17:50:11 +0530

    Add project files.

---
+++ CQRSMediatRDemos/Data/AppDbContext.cs
@@ -0,0 +1,11 @@
+﻿
+using CQRSMediatRDemos.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CQRSMediatRDemos.Data
+{
+    public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options)
+    {
+        public DbSet<Employee> Employees { get; set; }
+    }
+}
